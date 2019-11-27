12db31bcf885773ef58c947f8a15ad968bb662d6
jerry <sa517301@mail.ustc.edu.cn>
2019-11-27 19:58:19 +0800

    ...

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,13 +34,13 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('You can modify those links in your config file', '#'))
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 30
+DEFAULT_PAGINATION = False 
 
 # You can use the DISPLAY_PAGES_ON_MENU setting to control whether all those pages are displayed in the primary navigation menu. (Default is True.)
 DISPLAY_PAGES_ON_MENU = True

45b4512bd5ac32463cdea6813c3cec99b2c059ae
Jerry <sa517301@mail.ustc.edu.cn>
2021-03-23 02:27:49 +0000

    ...

--- pelicanconf.py
+++ pelicanconf.py
@@ -120,7 +120,7 @@ TAG_FEED_ATOM = None
 TAG_FEED_RSS = None
 
 # Comments
-ISSO_SITEURL = "http://www.jerrylsu.net/isso"
+ISSO_SITEURL = "http://www.jerrylsu.net"
 # DISQUS_SITENAME = "blog-notmyidea"
 
 # THEMES

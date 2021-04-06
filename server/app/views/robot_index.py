8ec9c0ae4be9bbcc9e93c854a8303347af8f727d
Jerry <sa517301@mail.ustc.edu.cn>
2021-04-06 09:54:49 +0000

    ...

--- server/app/views/robot_index.py
+++ server/app/views/robot_index.py
@@ -5,10 +5,6 @@ import json
 
 
 class RobotHandler(tornado.web.RequestHandler):
-    """构建一个向微博搜索API发送HTTP请求的简单Web应用。
-    这个Web应用有一个参数q作为查询字符串，并确定多久会出现
-    一条符合搜索条件的推文被发布在微博上（"每秒推数"）
-    """
     async def get(self):
         msg = self.get_argument("msg")
         # tornado 5.0以后只能在服务中使用异步客户端AsyncHTTPClient，非HTTPClient。
